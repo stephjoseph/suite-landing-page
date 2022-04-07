d003a245cb73edd073b422fa665ac6c651cc691b
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-04-07 15:54:26 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App flex w-full max-w-[40rem] flex-col items-center font-['Epilogue'] md:max-w-[50.625rem] xl:max-w-[69.375rem]">
+    <div className="App flex w-full max-w-[47.938rem] flex-col items-center font-['Epilogue'] md:max-w-[79.938rem] xl:max-w-[95.938rem]">
       <Header />
       <Main />
       <Footer />
